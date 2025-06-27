dbdb47422908f6c311f815dd600973f39655d9a6
Dư Văn Sang <sang25112005@gmail.com>
2025-06-27 08:32:23 +0700

    Upload Source Code

---
+++ quanlisach/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quanlisach.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
