a5ebde85fe7414f1e33743afc801f4c1a6d4a261
Dư Văn Sang <sang25112005@gmail.com>
2025-06-27 08:40:51 +0700

    add Jenkinsfile

--- obj/Debug/net8.0/quanlisach.AssemblyInfo.cs
+++ obj/Debug/net8.0/quanlisach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quanlisach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f92928834a5f32821d9f72ff4a012c10a641aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd2589e1987315862e69f63fe0adbbe458460e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("quanlisach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quanlisach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
